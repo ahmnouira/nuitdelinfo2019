6f0e0c7e3a00198c7a39caaa8f59e75e3ed9e5ef
Ahmed Nouira <ahmnouira@gmail.com>
2019-12-06 01:51:42 +0100

    add template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
- 
-  {path: '', component : HomeComponent, children :[
-
-    {path: '', loadChildren: './home/module-home/module-home.module#ModuleHomeModule'},
-    {path:'blog', loadChildren: './home/module-blog/module-blog.module#ModuleBlogModule'},
-    {path:'login', loadChildren: './home/module-login/module-login.module#ModuleLoginModule'},
-    {path:'register', loadChildren: './home/module-register/module-register.module#ModuleRegisterModule'},
-    {path:'contact', loadChildren: './home/module-contact/module-contact.module#ModuleContactModule'}
-  
-  ]},
 
 ]
 
